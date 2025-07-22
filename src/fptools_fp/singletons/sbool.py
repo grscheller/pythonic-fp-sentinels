7ac00d64a3773583614553abced72c8eb989000c
grscheller <geoffrey@scheller.com>
2025-07-21 23:34:55 -0600

    Moved singletons.py from fptools.into new singletons package.
    - also incorporated bool.py into the package

---
+++ src/fptools_fp/singletons/sbool.py
@@ -0,0 +1,125 @@
+# Copyright 2023-2025 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Class `SBool` - Subclassable Singleton Booleans"""
+
+from __future__ import annotations
+
+from typing import Final
+
+__all__ = ['SBool', 'Truth', 'Lie', 'TRUTH', 'LIE']
+
+
+class SBool(int):
+    """Subclassable Boolean-like class.
+
+    This class's sub-types represent different "flavors" of "truth"
+    where each flavor has one unique "truthy" and one unique "falsy"
+    instance.
+
+    .. note::
+        
+        Python does not permit bool to be subclassed, but ``int`` can
+        be. Under-the-hood a ``bool`` is just an ``int``. This class
+        inherits from ``int`` and relies on the underlying truthiness
+        and falsiness of ``1`` and ``0``.
+
+    .. important::
+
+        Only use SBool as a type, never as a constructor.
+
+    """
+
+    __slots__ = ('_flavor',)
+
+    def __new__(cls) -> SBool:
+        return super(SBool, cls).__new__(cls, 0)
+
+    def __repr__(self) -> str:
+        if self:
+            return 'SBool(1)'
+        return 'SBool(0)'
+
+    def flavor(self) -> str:
+        raise NotImplementedError
+
+
+class Truth(SBool):
+    """Truthy singleton SBool subclass.
+
+    .. note::
+        When using type hints, declare variables SBool, not Truth.
+
+    """
+
+    _instances: dict[str, Truth] = dict()
+
+    def __new__(cls, flavor: str = 'DEFAULT_TRUTH') -> Truth:
+        if flavor not in cls._instances:
+            cls._instances[flavor] = super(SBool, cls).__new__(cls, 1)
+        return cls._instances[flavor]
+
+    def __init__(self, flavor: str = 'DEFAULT_TRUTH') -> None:
+        self._flavor = flavor
+
+    def __repr__(self) -> str:
+        return f"Truth('{self._flavor}')"
+
+    def flavor(self) -> str:
+        return self._flavor
+
+
+class Lie(SBool):
+    """Falsy singleton SBool subclass.
+
+    .. note::
+        When using type hints, declare variables SBool, not Lie.
+
+    """
+
+    _instances: dict[str, Lie] = dict()
+
+    def __new__(cls, flavor: str = '') -> Lie:
+        if flavor not in cls._instances:
+            cls._instances[flavor] = super(SBool, cls).__new__(cls, 0)
+        return cls._instances[flavor]
+
+    def __init__(self, flavor: str = '') -> None:
+        self._flavor = flavor
+
+    def __repr__(self) -> str:
+        return f"Lie('{self._flavor}')"
+
+    def flavor(self) -> str:
+        return self._flavor
+
+
+TRUTH: Final[Truth] = Truth()
+LIE: Final[Lie] = Lie()
+
+
+def snot(val: SBool) -> SBool:
+    """Return the opposite truthiness of the same flavor of truth.
+
+    .. note::
+
+        Trying to use the Python ``not`` operator for this will just
+        return a ``bool``. There is no ``__not__`` dunder method
+        that will change the behavior of ``not``.
+
+    """
+    if val:
+        return Lie(val.flavor())
+    return Truth(val.flavor())
